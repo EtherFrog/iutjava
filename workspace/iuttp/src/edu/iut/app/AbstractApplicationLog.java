e454906a190d39bbb15152a0e3dd23ad29959e29
EtherFrog <joe.deoliveira@yahoo.fr>
2015-11-10 10:17:59 +0100

    changes to git ignore

---
+++ workspace/iuttp/src/edu/iut/app/AbstractApplicationLog.java
@@ -0,0 +1,45 @@
+package edu.iut.app;
+
+import java.util.ArrayList;
+
+public abstract class AbstractApplicationLog implements IApplicationLog {
+
+	protected String message;
+
+	protected ArrayList<IApplicationLogListener> listeners;
+
+	public AbstractApplicationLog() {
+		message = null;
+		listeners = new ArrayList<IApplicationLogListener>();
+
+	}
+
+	public void AddLogListener(IApplicationLogListener listener){
+
+		listeners.add(listener);
+
+	}
+
+	public ArrayList<IApplicationLogListener> GetListeners(){
+
+		return listeners;
+
+	}
+
+	/** TP1 : Fonction venant de l'interface par héritage */
+	@Override
+	public abstract void setMessage(String message);
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+	
+	
+	/** Listener action */
+	protected void fireMessage(String level, String message) {
+		for (IApplicationLogListener listener_i : listeners) {
+			listener_i.newMessage(level, message);
+		}
+	}
+}
