4101993a57914f14347ded809baf4cd3947ad790
johnathan95 <johnathan.nadarajah@u-psud.fr>
2015-11-17 11:00:07 +0100

    update tp3

--- workspace/iuttp/src/edu/iut/app/ApplicationSession.java
+++ workspace/iuttp/src/edu/iut/app/ApplicationSession.java
@@ -7,26 +7,34 @@ import java.util.logging.Logger;
 
 public class ApplicationSession {
 	
-	// Exercice 1 : Gérer l'internationation
 	protected ResourceBundle resourceBundle;
 	protected Locale locale;
-	
-	// Exercice 2 : Logger
 	protected Logger sessionGuiLogger;
 	protected Logger sessionExceptionLogger;
+	protected String[] months;
+	protected String[] days;
 
 
 	private static ApplicationSession session = null;
 	private ApplicationSession() {
-		/* Defini US comme locale par défaut */
-		Locale.setDefault(Locale.US);
-		String resourceBunndle="edu.iut.resources.strings";
 		locale = Locale.getDefault();
-		resourceBundle = ResourceBundle.getBundle("edu.iut.resources.strings.res_fr.properties");
-		sessionGuiLogger = Logger.getLogger("GUI",resourceBunndle);
-		sessionGuiLogger.setLevel(Level.ALL);/* Touls les message doivent être affiché */
-		sessionExceptionLogger = Logger.getLogger("GUIexp");
+		resourceBundle = ResourceBundle.getBundle("edu.iut.resources.strings.res");
+		sessionGuiLogger = Logger.getLogger("IUTTrain");
+		sessionGuiLogger.setLevel(Level.ALL);
+		sessionExceptionLogger = Logger.getLogger("IUTException");
 		sessionExceptionLogger.setLevel(Level.ALL);
+		
+		days = new String[7];
+		days[0] = getString("monday"); days[1] =  getString("tuesday"); days[2] =  getString("wednesday");
+		days[3] = getString("thursday");   days[4] =  getString("friday"); days[5] =  getString("saturday");
+		days[6] = getString("sunday"); 
+		
+		months = new String[12];
+		months[0] = getString("january"); months[1] =  getString("february"); months[2] =  getString("march");
+		months[3] = getString("april");   months[4] =  getString("may");      months[5] =  getString("june");
+		months[6] = getString("july"); months[7] =  getString("august"); months[8] =  getString("september");
+		months[9] = getString("october"); months[10] =  getString("november"); months[11] =  getString("december");
+		
 		
 	}
 	
@@ -47,13 +55,19 @@ public class ApplicationSession {
 	
 	public void setLocale(Locale locale){
 		this.locale = locale;
-		Locale.setDefault(Locale.US);
-		/* récupère les resources */
-		resourceBundle= ResourceBundle.getBundle("edu.iut.resources.strings.res.properties");
+		Locale.setDefault(this.locale);
+		resourceBundle=ResourceBundle.getBundle("edu.iut.resources.strings.res");
 	}
 	
 	public String getString(String key) {
 		return resourceBundle.getString(key);
+	}
+	
+	public String[] getDays() {
+		return days;
+	}
+	public String[] getMonths() {
+		return months;
 	}
 	
 	
---
+++ workspace/iuttp/src/edu/iut/app/CommandLineOption.java
@@ -0,0 +1,67 @@
+package edu.iut.app;
+
+import java.io.File;
+
+public class CommandLineOption<ValueType> {
+	
+	public enum OptionType{
+		NONE("None"),
+		FILE("File"),
+		STRING("String"),
+		INTEGER("Integer"),
+		DOUBLE("Double"),
+		NOVALUE("NoValue");
+		private String optionType;
+		
+		OptionType(String optionType) {
+			this.optionType = optionType;
+		}
+		
+		public String toString() {
+			return optionType;
+		}		
+	}
+	
+	public CommandLineOption() {		
+		/* EX1: Initializer les attributs */
+		 //Variable d'instance
+		ValueType value;
+	}
+	public CommandLineOption(final OptionType optionType, final String key, final String description, final File file) {
+		/* EX1 : Affecter les attributs */
+		this.optionType=optionType;
+		this.key=key;
+		this.description=description;
+		
+	}
+	public void setOption(OptionType optionType, String key, String description, ValueType defaultValue) {
+		/* EX1 : Affecter les attributs */
+	}
+	public  void setValue(ValueType value) {
+		this.value = value;
+	}
+		
+	public String getKey() {
+		return this.key;
+	}
+	public String getDescription() {
+		return this.description;
+	}
+	public ValueType getValue() {
+		if (value != null) {
+			return value;
+		}
+		return defaultValue;
+	}
+	public OptionType getOptionType() {
+		return optionType;
+	}
+
+	protected String key;
+	protected String description;
+	protected ValueType defaultValue;
+	protected ValueType value;
+	protected OptionType optionType;
+	
+
+}
